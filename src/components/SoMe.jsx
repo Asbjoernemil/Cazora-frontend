4a38c667c388200ca65cf03f23ed1a8d5c6e7d9d
Tunafan <124045808+Tunafan@users.noreply.github.com>
2023-12-01 11:46:45 +0100

    Update SoMe.jsx

--- src/components/SoMe.jsx
+++ src/components/SoMe.jsx
@@ -2,12 +2,12 @@
 
 export default function SoMe () {
     return (
-        <div>
+        <footer>
 			<h6>See more at our instagram page:</h6>
 				<a className="logo" href="https://www.instagram.com/cazora_vintage/">
-							<img src= "../../cazora C.png" className="icon-instagram">
+							<img src= "../../cazora C.png" className="w-1/5">
 							</img>
 				</a>
-            </div>
+            </footer>
     )
 }
