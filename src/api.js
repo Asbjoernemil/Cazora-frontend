b5e7d407dd64ed9568d74a23f40861254d200b51
Asbjoernemil <asbjoernemil24@gmail.com>
2023-12-14 20:50:08 +0100

    AZURE CONNECT

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 // api.js
-export const BASE_URL = 'http://localhost:3000'; // vores backend URL
+export const BASE_URL = 'https://cazoravintage.azurewebsites.net/'; // vores backend URL
 
 export const getCategories = async () => {
     const response = await fetch(`${BASE_URL}/categories`);
